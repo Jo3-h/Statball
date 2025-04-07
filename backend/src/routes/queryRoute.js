03264c63f04bcdc84670f3a3b33291d2e43a3950
Jo3-h <joe.hosking1@gmail.com>
2025-04-07 18:43:45 +0930

    build test routes and defined the file for handing query from the frontend

---
+++ backend/src/routes/queryRoute.js
@@ -0,0 +1,13 @@
+// queryRoute.js
+
+const express = require("express");
+const router = express.Router();
+const logger = require("../config/logger");
+
+router.post("/api/query", async (req, res) => {
+  const { question } = req.body;
+
+  // test whether the frontend is correctly passing the query
+  logger.info("Received query: ", question);
+  res.status(200).json({ message: "Query received", question });
+});
---
+++ backend/src/routes/testRoute.js
@@ -0,0 +1,10 @@
+// testRoute
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "Backend Server is running!" });
+});
+
+module.exports = router;
