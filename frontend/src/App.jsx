2cc972a31f4ce462d83ffda6a2eb44b774c8e962
Jo3-h <joe.hosking1@gmail.com>
2025-04-07 20:42:13 +0930

    built frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [query, setQuery] = useState("");
+
+  const processQuery = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/api/query", {
+        method: "POST",
+        body: JSON.stringify({ question: query }),
+      });
+
+      // check if the response is ok
+      if (!response.ok) {
+        throw new Error("Error in response");
+      }
+
+      const data = await response.json();
+      console.log("Response from server:", data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-blue-400 absolute top-0 left-0 flex flex-col items-center justify-center">
+        <div
+          className="h-15 w-160 bg-white rounded-lg p-3 flex"
+          id="text-box-div"
+        >
+          <textarea
+            className="w-full h-full bg-yellow-100 flex align-middle justify-center text-center rounded-md"
+            placeholder="  Query AFL Data"
+          ></textarea>
+          <button className="" onClick={processQuery}></button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
