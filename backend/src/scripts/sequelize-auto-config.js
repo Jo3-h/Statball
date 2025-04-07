01662098fb92d96eae513834e82f80a2e32838ee
Jo3-h <joe.hosking1@gmail.com>
2025-04-07 17:50:43 +0930

    debug model generation script

--- backend/src/scripts/sequelize-auto-config.js
+++ backend/src/scripts/sequelize-auto-config.js
@@ -1,6 +1,13 @@
 const SequelizeAuto = require("sequelize-auto");
 require("dotenv").config();
 
+// Print out the environment variables
+console.log("DB_NAME:", process.env.DEV_DB_NAME);
+console.log("DB_USERNAME:", process.env.DEV_DB_USERNAME);
+console.log("DB_PASSWORD:", process.env.DEV_DB_PASSWORD);
+console.log("DB_HOST:", process.env.DEV_DB_HOST);
+console.log("DB_DIALECT:", process.env.DEV_DB_DIALECT);
+
 const auto = new SequelizeAuto(
   process.env.DEV_DB_NAME,
   process.env.DEV_DB_USERNAME,
